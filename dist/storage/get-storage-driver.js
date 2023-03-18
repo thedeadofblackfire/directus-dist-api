586bb67d0285fcf77fd1bdc39a06c3a5fd8d9e71
Nick <thedeadofblackfire@yahoo.com>
2023-03-18 15:01:21 +0100

    Update get-storage-driver.js

--- dist/storage/get-storage-driver.js
+++ dist/storage/get-storage-driver.js
@@ -2,12 +2,21 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getStorageDriver = exports._aliasMap = void 0;
 exports._aliasMap = {
+    local: '/var/task/node_modules/@directus/storage-driver-local',
+    s3: '/var/task/node_modules/@directus/storage-driver-s3',
+    gcs: '@directus/storage-driver-gcs',
+    azure: '@directus/storage-driver-azure',
+    cloudinary: '@directus/storage-driver-cloudinary',
+};
+/*
+exports._aliasMap = {
     local: '@directus/storage-driver-local',
     s3: '@directus/storage-driver-s3',
     gcs: '@directus/storage-driver-gcs',
     azure: '@directus/storage-driver-azure',
     cloudinary: '@directus/storage-driver-cloudinary',
 };
+*/
 const getStorageDriver = async (driverName) => {
     if (driverName in exports._aliasMap) {
         driverName = exports._aliasMap[driverName];
