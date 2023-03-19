b65f50f399b6a8531b6c6221f9969ef6c594ec48
Nick <thedeadofblackfire@yahoo.com>
2023-03-19 09:50:12 +0100

    try 4

--- dist/storage/register-drivers.js
+++ dist/storage/register-drivers.js
@@ -3,6 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.registerDrivers = void 0;
 const env_1 = require("../env");
 const get_storage_driver_1 = require("./get-storage-driver");
+
+const storageDrivers_local = require("@directus/storage-driver-local");
+const storageDrivers_s3 = require("@directus/storage-driver-s3");
+
 const registerDrivers = async (storage) => {
     const env = (0, env_1.getEnv)();
     const usedDrivers = [];
@@ -13,11 +17,23 @@ const registerDrivers = async (storage) => {
             usedDrivers.push(value);
     }
 	console.log('usedDrivers', usedDrivers);
+	
+	/*
+	import formatTitle from '@directus/format-title';
+	storage_local.default
+	storage_s3.default
+	*/
+	
+	storage.registerDriver('local', storageDrivers_local.default);
+	storage.registerDriver('s3', storageDrivers_s3.default);
+	
+	/*
     for (const driverName of usedDrivers) {
         const storageDriver = await (0, get_storage_driver_1.getStorageDriver)(driverName);
         if (storageDriver) {
             storage.registerDriver(driverName, storageDriver);
         }
     }
+	*/
 };
 exports.registerDrivers = registerDrivers;
